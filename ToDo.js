14a5c37226b1656e72e7bd332678081b2629480e
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-08 22:48:07 +0900

    Saving To Dos part two

--- ToDo.js
+++ ToDo.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from 'react-native';
+import PropTypes from 'prop-types';
 
 const { width, height } = Dimensions.get('window');
 
 export default class ToDo extends React.Component {
-	state = {
-		isEditing: false,
-		isCompleted: false,
-		toDoValue: ''
+	constructor(props) {
+		super(props);
+		this.state = { isEditing: false, toDoValue: props.text };
+	}
+	static propTypes = {
+		text: PropTypes.string.isRequired,
+		isCompleted: PropTypes.bool.isRequired,
+		deleteToDo: PropTypes.func.isRequired,
+		id: PropTypes.string.isRequired
 	};
+
 	render() {
 		const { isEditing, isCompleted, toDoValue } = this.state;
-		const { text } = this.props;
+		const { text, id, deleteToDo } = this.props;
 		return (
 			<View style={styles.container}>
 				<View style={styles.column}>
@@ -54,7 +61,7 @@ export default class ToDo extends React.Component {
 								<Text style={styles.actionText}>✏️</Text>
 							</View>
 						</TouchableOpacity>
-						<TouchableOpacity>
+						<TouchableOpacity onPressOut={() => deleteToDo(id)}>
 							<View style={styles.actionContainer}>
 								<Text style={styles.actionText}>❌</Text>
 							</View>
@@ -72,10 +79,8 @@ export default class ToDo extends React.Component {
 		});
 	};
 	_startEditing = () => {
-		const { text } = this.props;
 		this.setState({
-			isEditing: true,
-			toDoValue: text
+			isEditing: true
 		});
 	};
 	_finishEditing = () => {
@@ -126,8 +131,7 @@ const styles = StyleSheet.create({
 	column: {
 		flexDirection: 'row',
 		alignItems: 'center',
-		width: width / 2,
-		justifyContent: 'space-between'
+		width: width / 2
 	},
 	actions: {
 		flexDirection: 'row'
