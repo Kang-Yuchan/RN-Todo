5ea94c3ea6afbc3ace01bb8dafbc7f5428f458ad
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-08 19:25:57 +0900

    Styling To Do Component

--- ToDo.js
+++ ToDo.js
@@ -9,13 +9,41 @@ export default class ToDo extends React.Component {
 		isCompleted: false
 	};
 	render() {
-		const { isCompleted } = this.state;
+		const { isEditing, isCompleted } = this.state;
 		return (
 			<View style={styles.container}>
-				<TouchableOpacity onPress={this._toggleComplete}>
-					<View style={[ styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle ]} />
-				</TouchableOpacity>
-				<Text style={styles.text}>Hello Im ToDo</Text>
+				<View style={styles.column}>
+					<TouchableOpacity onPress={this._toggleComplete}>
+						<View
+							style={[ styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle ]}
+						/>
+					</TouchableOpacity>
+					<Text style={[ styles.text, isCompleted ? styles.completedText : styles.uncompletedText ]}>
+						Hello Im ToDo
+					</Text>
+				</View>
+				{isEditing ? (
+					<View style={styles.actions}>
+						<TouchableOpacity onPressOut={this._finishEditing}>
+							<View style={styles.actionContainer}>
+								<Text style={styles.actionText}>✅</Text>
+							</View>
+						</TouchableOpacity>
+					</View>
+				) : (
+					<View style={styles.actions}>
+						<TouchableOpacity onPressOut={this._startEditing}>
+							<View style={styles.actionContainer}>
+								<Text style={styles.actionText}>✏️</Text>
+							</View>
+						</TouchableOpacity>
+						<TouchableOpacity>
+							<View style={styles.actionContainer}>
+								<Text style={styles.actionText}>❌</Text>
+							</View>
+						</TouchableOpacity>
+					</View>
+				)}
 			</View>
 		);
 	}
@@ -26,6 +54,16 @@ export default class ToDo extends React.Component {
 			};
 		});
 	};
+	_startEditing = () => {
+		this.setState({
+			isEditing: true
+		});
+	};
+	_finishEditing = () => {
+		this.setState({
+			isEditing: false
+		});
+	};
 }
 
 const styles = StyleSheet.create({
@@ -34,7 +72,8 @@ const styles = StyleSheet.create({
 		borderBottomColor: '#bbb',
 		borderBottomWidth: StyleSheet.hairlineWidth,
 		flexDirection: 'row',
-		alignItems: 'center'
+		alignItems: 'center',
+		justifyContent: 'space-between'
 	},
 	circle: {
 		width: 30,
@@ -53,5 +92,25 @@ const styles = StyleSheet.create({
 		fontWeight: '600',
 		fontSize: 20,
 		marginVertical: 20
+	},
+	completedText: {
+		color: '#bbb',
+		textDecorationLine: 'line-through'
+	},
+	uncompletedText: {
+		color: '#f23657'
+	},
+	column: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		width: width / 2,
+		justifyContent: 'space-between'
+	},
+	actions: {
+		flexDirection: 'row'
+	},
+	actionContainer: {
+		marginVertical: 10,
+		marginHorizontal: 10
 	}
 });
